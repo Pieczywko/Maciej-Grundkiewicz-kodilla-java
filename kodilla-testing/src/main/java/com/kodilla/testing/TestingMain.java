aa1267f98a03ec7d41a8d83c336190ed735c1b0c
Maciej Grundkiewicz <maciej.grundkiewicz@gmail.com>
2022-04-27 21:37:04 +0200

    błąd z mainem

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -4,21 +4,4 @@ import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
 
-    public static void main(String[] args) {
-        Calculator calculator= new Calculator();
-
-        int add = calculator.add(10,10);
-        if (add == 20) {
-            System.out.println("test ok");
-        }else{
-            System.out.println("Error!");
-        }
-        int subtract = calculator.subtract(10,10);
-        if (subtract== 0) {
-            System.out.println("test OK");
-        }else{
-            System.out.println("Error!");
-        }
-
-    }
-}
+    public static void main(String[] args)
